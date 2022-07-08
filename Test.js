d7d4a93c2ae532e706c26e33888b544227e7bf54
Shweta2170 <108939374+Shweta2170@users.noreply.github.com>
2022-07-08 19:11:09 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+switch (script) {
+  case 'build':
+  case 'eject':
+  case 'start':
+  case 'test': {
+    const result = spawn.sync(
+      'node',
+      [require.resolve('../scripts/' + script)].concat(args),
+      { stdio: 'inherit' }
+    );
 ReactDOM.render(
   <App />,
   document.getElementById('root')
